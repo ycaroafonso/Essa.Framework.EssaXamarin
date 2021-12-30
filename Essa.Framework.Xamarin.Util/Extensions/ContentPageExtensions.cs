26a6041b603db522f693b5e4d7a776ce5097a3a4
Ycaro Afonso <ycaroafonso@outlook.com>
2021-12-29 22:33:21 -0400

    nova versão

--- Essa.Framework.Xamarin.Util/Extensions/ContentPageExtensions.cs
+++ Essa.Framework.Xamarin.Util/Extensions/ContentPageExtensions.cs
@@ -16,5 +16,14 @@
         {
             await page.DisplayAlertAsync(displayAlert.Mensagem);
         }
+
+        public static async Task DisplayAlertAsync(this Page page, IDisplayAlert displayAlert)
+        {
+            await page.DisplayAlert(displayAlert.title, displayAlert.message, displayAlert.cancel);
+        }
+        public static async Task DisplayAlertAsync(this Page page, IRetornarMensagemDisplayAlert displayAlert)
+        {
+            await page.DisplayAlertAsync(displayAlert.Mensagem);
+        }
     }
 }
